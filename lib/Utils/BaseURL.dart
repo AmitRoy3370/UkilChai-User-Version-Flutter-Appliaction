e1200ab1d8492d7199144a7636fa6e474446af60
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2026-04-01 22:12:00 +0600

    Updated files before pulling latest changes

--- lib/Utils/BaseURL.dart
+++ lib/Utils/BaseURL.dart
@@ -1,5 +1,6 @@
 class Urls {
 
-  final String baseURL = "https://ukilchaiuserversion.onrender.com/api/";
+  //final String baseURL = "https://ukilchaiuserversion.onrender.com/api/";
+  final String baseURL = "http://178.104.120.196:8080/api/";
 
 }
